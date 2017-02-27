e982375c22a8aa7b2acf02b5e132e3dfd5f22e4c
Tom Tsui <tom.tsui@oocl.com>
2017-02-27 22:05:14 +0800

    Fix anti-clockwise logic in polygon

--- IoTApi/components/geo-controller.js
+++ IoTApi/components/geo-controller.js
@@ -7,11 +7,7 @@ var responseFactory = require('../util/response-factory');
 
 exports.getGeoFenceInBound = function(req, res) {
 	convertPolygonToClockwise({ coords: { coordinates: req.body } });
-	// var querySpec = {
-	// 	query:
-	// 	, parameters: { name: '@search', value: { type: 'Polygon', coordinates: [req.body] } }
-	// };
-	//
+	
 	var coordStr = JSON.stringify({ type: 'Polygon', coordinates: [req.body] }, false, null);
 	var result = [];
 	// Document DB Client not support binding Object parameter
@@ -110,31 +106,35 @@ exports.createGeoFenceLocations = function(req, res) {
 	);
 };
 
-// Document DB only support clockwise Polygon coordinates
+// Document DB only support anti-clockwise Polygon coordinates, clockwise implies exclusion
 // http://stackoverflow.com/questions/1165647/how-to-determine-if-a-list-of-polygon-points-are-in-clockwise-order
 // https://en.wikipedia.org/wiki/Shoelace_formula
 function convertPolygonToClockwise(shape) {
 	if (shape.geoType === 'circle') { return; }
-	//console.log('Before: ' + shape.coords.coordinates);
-	var isClockwise = false;
-	// Should not attempt more than number of vertex in polygon
-	for (var count = 0; count < shape.coords.coordinates.length && !isClockwise; count++) {
-		var sum = 0;
-		for (var n = shape.coords.coordinates.length - 1; n >= 1; n--) {
-			var nthPoint = shape.coords.coordinates[n];
-			var nthMinusOnePoint = shape.coords.coordinates[n - 1];
-			sum += (nthPoint[0] - nthMinusOnePoint[0]) * (nthPoint[1] - nthMinusOnePoint[1]);
-		}
-		if (sum < 0) {
-			isClockwise = true
-		} else {
-			// Shift the points by 1 position and recalculate
-			shape.coords.coordinates.splice(0, 0, shape.coords.coordinates.pop());
-		}
+
+	if (isPolygonClockwise(shape.coords.coordinates)) {
+		_.reverse(shape.coords.coordinates);
 	}
 	shape.coords.coordinates.push(_.cloneDeep(shape.coords.coordinates[0]));
 	shape.coords.coordinates = [shape.coords.coordinates];
-	//console.log('After: ' + shape.coords.coordinates);
+}
+
+function isPolygonClockwise(coordinates) {
+	var signedArea = 0;
+	var x1, y1, x2, y2;
+	for (var n = 0; n < coordinates.length; n++) {
+		x1 = coordinates[n][0];
+		y1 = coordinates[n][1];
+		if (n == coordinates.length - 1) {
+			x2 = coordinates[0][0];
+			y2 = coordinates[0][1];
+		} else {
+			x2 = coordinates[n+1][0];
+			y2 = coordinates[n+1][1];
+		}
+		signedArea += (x1 * y2 - x2 * y1);
+	}
+	return signedArea < 0;
 }
 
 // TODO Validation
