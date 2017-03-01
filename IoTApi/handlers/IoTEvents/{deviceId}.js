7a4744842f63f3149340230a78eb410fba074efa
Tom Tsui <tom.tsui@oocl.com>
2017-03-01 09:22:38 +0800

    Update date type for query in paging, fixed some handle cases

--- IoTApi/handlers/IoTEvents/{deviceId}.js
+++ IoTApi/handlers/IoTEvents/{deviceId}.js
@@ -18,13 +18,13 @@ function queryEventByDeviceId(req, res, next) {
 		[
 			function(callback) {
 				var querySpec = {
-					query: `SELECT TOP ${topLimit} * FROM ${config.collection.events} r WHERE r.IoTHub.ConnectionDeviceId = @deviceId AND r.hAccuracy <= '500' AND r.hAccuracy > '-1' AND r.timeStamp <= @timeStamp ORDER BY r.timeStamp DESC`
+					query: `SELECT TOP ${topLimit} * FROM ${config.collection.events} r WHERE r.IoTHub.ConnectionDeviceId = @deviceId AND r.hAccuracy <= 500 AND r.hAccuracy > -1 AND r.timeStamp <= @timeStamp ORDER BY r.timeStamp DESC`
 					, parameters: [
 						{ name: '@deviceId', value: req.params.deviceId }
-						, { name: '@timeStamp', value: `${req.query.timeStamp}` }
+						, { name: '@timeStamp', value: req.query.timeStamp }
 					]
 				};
-				// console.log(querySpec);
+				// //console.log(querySpec);
 				common.queryCollection(config.collection.events, querySpec, pageSize, callback);
 			}
 			, function(cursor, callback) {
@@ -32,7 +32,7 @@ function queryEventByDeviceId(req, res, next) {
 			}
 			, function(fetchedRecords, callback) {
 				var querySpec = {
-					query: `SELECT COUNT(r) COUNT FROM ${config.collection.events} r WHERE r.IoTHub.ConnectionDeviceId = @deviceId AND r.hAccuracy <= '500' AND r.hAccuracy > '-1'`
+					query: `SELECT COUNT(r) COUNT FROM ${config.collection.events} r WHERE r.IoTHub.ConnectionDeviceId = @deviceId AND r.hAccuracy <= 500 AND r.hAccuracy > -1`
 					, parameters: [
 						{ name: '@deviceId', value: req.params.deviceId }
 					]
@@ -56,24 +56,33 @@ function queryEventByDeviceId(req, res, next) {
 }
 
 function fetchRecord(cursor, rowToSkip, pageSize, resultHolder, callback) {
+	//console.log('fetchRecord');
 	cursor.executeNext(function(err, nextBatch) {
 		if (err) {
 			return callback(err);
 		}
 
 		if (rowToSkip > nextBatch.length && nextBatch.length == pageSize) {
+			//console.log('A');
 			if (nextBatch.length < pageSize) {
+				//console.log('A1.1');
 				return callback(undefined, []);
 			} else {
+				//console.log('A1.2');
 				fetchRecord(cursor, rowToSkip - pageSize, pageSize, resultHolder, callback);
 			}
 		} else {
+			//console.log('B');
 			if (nextBatch.length == pageSize) {
+				//console.log('B1');
 				if (rowToSkip == 0) {
+					//console.log('B1.1');
 					if (resultHolder.length < pageSize) {
+						//console.log('B1.1.1');
 						resultHolder = _.concat(resultHolder, _.takeRight(nextBatch, pageSize - resultHolder.length));
 						fetchRecord(cursor, 0, pageSize, resultHolder, callback);
 					} else {
+						//console.log('B1.1.2');
 						var fetchCompleted = false;
 						var groupDate = resultHolder[resultHolder.length - 1].groupDate;
 
@@ -92,13 +101,18 @@ function fetchRecord(cursor, rowToSkip, pageSize, resultHolder, callback) {
 						}
 					}
 				} else {
+					//console.log('B1.2');
 					resultHolder = _.concat(resultHolder, _.takeRight(nextBatch, pageSize - rowToSkip));
 					fetchRecord(cursor, 0, pageSize, resultHolder, callback);
 				}
 			} else {
+				//console.log('B2');
 				if (nextBatch.length > rowToSkip) {
-					return callback(undefined, _.takeRight(nextBatch, nextBatch.length - rowToSkip));
+					//console.log('B2.1');
+					resultHolder = _.concat(resultHolder, _.takeRight(nextBatch, nextBatch.length - rowToSkip));
+					return callback(undefined, resultHolder);
 				} else {
+					//console.log('B2.2');
 					return callback(undefined, []);
 				}
 			}
